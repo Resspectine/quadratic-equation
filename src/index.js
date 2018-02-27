37a9cc0672a73170ef7c3a91e23404980cf1825d
Resspectine <resspectyn@gmail.com>
2018-02-27 22:48:00 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+    let reg = /[-+]? ?\d+/g;
+    let regS = /\s/;
+    let parsed = equation.match(reg);
+    parsed[2] = parsed[2].replace(regS, '');
+    parsed[3] = parsed[3].replace(regS, '');
+    let a = parseInt(parsed[0], 10);
+    let b = parseInt(parsed[2], 10);
+    let c = parseInt(parsed[3], 10);
+    let x1 = Math.round((-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a));
+    let x2 = Math.round((-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a));
+    let out;
+    x1 > x2 ? out = [x2, x1] : out = [x1, x2];
+    return out;
+};
